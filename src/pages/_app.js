53a57cb014a828fba0d347a760d7ed535f1fef7e
Latif <latifbaraba88@gmail.com>
2023-03-08 20:54:39 +0700

    1st commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
+import { store } from '@/store/index.'
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { Provider } from 'react-redux'
+
+// Create a client
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider>
+            <QueryClientProvider client={queryClient}>
+                <Provider store={store}>
+                    <Component {...pageProps} />
+                </Provider>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </ChakraProvider>
+    )
 }
